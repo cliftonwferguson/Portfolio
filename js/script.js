1701cd94be1e9abc1daebb8f829fbff624d81b18
Clifton Ferguson <clifton.ferguson7@gmail.com>
2019-12-01 18:09:15 -0600

    sticky

--- js/script.js
+++ js/script.js
@@ -29,6 +29,28 @@ $(document).ready(function() {
               items:5
           }
       }
-  })
+  });
+
+
+
+  const nav = $("#navigation");
+  const navTop = nav.offset().top;
+
+  $(window).on("scroll", stickyNavigation);
+
+  function stickyNavigation() {
+
+    var body = $("body");
+
+    if($(window).scrollTop() >= navTop) {
+      body.addClass("fixedNav");
+    }
+    else {
+      body.removeClass("fixedNav");
+    }
+
+  }
+
+
 
 });
